504833aee321beb4ef196970478779de32dd89f2
merkjinx <merkjinx@users.noreply.github.com>
2017-03-17 17:54:52 -0700

    Update listener.py

--- listener.py
+++ listener.py
@@ -1,4 +1,25 @@
-# : ( nothing is here yet
 import sys
-for line in sys.stdin:
-    print line
+import getopt
+
+
+
+
+
+
+def main(argv):
+    whitelistunpared = ''
+    blacklistunpared = ''
+    opts, args = getopt.getopt(argv,"ha:b:",["ifile=","ofile="])
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'test.py -a <whitelist-array> -b <blacklist-array>'
+            sys.exit()
+        elif opt in ("-a", "--aa"):
+            whitelistunpared = arg
+        elif opt in ("-b", "--bb"):
+            blacklistunpared = arg
+#        print  "Whitelist:",whitelistunpared # Debug
+#        print  "Blacklist:",blacklistunpared # Debug
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
