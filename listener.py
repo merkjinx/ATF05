263636eeeaea5cf7d9b08e71e69ce4b8763fc593
merkjinx <merkjinx@users.noreply.github.com>
2017-03-18 03:49:34 -0700

    Update listener.py

--- listener.py
+++ listener.py
@@ -23,22 +23,68 @@ listenerthread = []
 notifications = 0
 
 foreignaddr = []
+foreignstatus = []
 
-
+actualhosts = [0] * 1500
+c = 0
 def listener_menu():
     #Going to add more here
     print "Booted into listener"
 def proccess_results(input):
-    
+    global blacklist
+    c = 0
     p = Popen(["grep -e tcp -e udp"], stdout=PIPE, stdin=PIPE, stderr=PIPE, shell=True)
     stdout_data = p.communicate(input=input)[0]
     p = Popen(["cut -c 22-70"], stdout=PIPE, stdin=PIPE, stderr=PIPE, shell=True)
     stdout_data = p.communicate(input=stdout_data)[0]
+    
+    teno = Popen(["cut -c 46-57"], stdout=PIPE, stdin=PIPE, stderr=PIPE, shell=True)
+    out_data = teno.communicate(input=stdout_data)[0]
+    
     p = Popen(["cut -c 1-42"], stdout=PIPE, stdin=PIPE, stderr=PIPE, shell=True)
     stdout_data = p.communicate(input=stdout_data)[0]
     
-    
+    foreignstatus = out_data.split()
     foreignaddr = stdout_data.split()
+    #print "addrlist:",len(foreignaddr)
+    for i in range(0, len(foreignaddr)):
+        #print "start range lqqq"
+        while c < len(actualhosts):
+            if actualhosts[c] == 0:
+                #print "Open Memory space found! @",c
+                if actualhosts[c] != foreignaddr[i]:
+                    c=c
+                    #print foreignaddr[i]
+                    #print "index:",i
+                    actualhosts[i] = foreignaddr[i]
+                    break
+                else:
+                    print "multiple connections from ",foreignaddr[i]," ip @",i
+            else:
+                #print "NO OPEN MEMORY SPACES @",c
+                c = c+1
+        c = c+1
+
+    filtered = sorted(set(actualhosts))
+    # filter out whitelist/blacklisted hosts
+    y = 0
+    for i in range(0, len(blacklist)):
+        while y < len(filtered):
+            if blacklist[i] == filtered[y]:
+                print "Warning BlackListed ip address has beeen detected"
+            else:
+                y = y+1
+    
+    
+    
+    
+    
+    #
+#print filtered
+
+
+
+    
 
     output=foreignaddr
     return output
@@ -58,6 +104,7 @@ def listen(som,sun):
 def main(argv):
     global whitelist
     global blacklist
+    #print blacklist
     opts, args = getopt.getopt(argv,"ha:b:n:n",["aa=","bb=","notice="])
     for opt, arg in opts:
         if opt == '-h':
@@ -73,7 +120,7 @@ def main(argv):
         #print  "Whitelist:",whitelistunpared # Debug
         #print  "Blacklist:",blacklistunpared # Debug
     whitelist = whitelistunpared.split()
-    blacklist = whitelistunpared.split()
+    blacklist = blacklistunpared.split()
     listener_menu()
 
     try:
